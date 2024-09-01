c7720d69a4389a021c77a15cc6ffe366c75b01d6
shivi028 <tiwarishivi028@gmail.com>
2024-09-02 01:48:19 +0530

    Build HeroFreelencer

---
+++ src/app/HeroFreelencer/page.tsx
@@ -0,0 +1,12 @@
+import HeroFreelencer from '@/components/HeroFreelencer/HeroFreelencer'
+import React from 'react'
+
+const HeroFree = () => {
+  return (
+    <div>
+      <HeroFreelencer/>
+    </div>
+  )
+}
+
+export default HeroFree
--- src/app/WorkCard/[id]/page.tsx
+++ src/app/WorkCard/[id]/page.tsx
@@ -24,7 +24,7 @@ const WorkCardDetail = () => {
         <div className=' flex '>
           <div className='flex flex-col gap-y-4 w-[50%]'>
             <div>
-              <Link href={'/'}>
+              <Link href={'/HeroFreelencer'}>
                 <ArrowLeftIcon className="h-8 w-8" />
               </Link>
             </div>
